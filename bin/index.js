bc154cb9a5f6f8dd24cefb4781339fa28c59d501
AndiKod <andrei@andikod.fr>
2023-11-12 23:10:33 +0100

    bien

--- bin/index.js
+++ bin/index.js
@@ -18,7 +18,7 @@ if (args.length < 1) {
   process.exit(1); //an error occurred
 }
 if (args.length > 1) {
-  console.error('Your project name should have a single word ;)');
+  console.error('Your project name should have a single word, or hyphens ;)');
   process.exit(1); //an error occurred
 }
 
@@ -48,7 +48,7 @@ inquirer
     {
       type: 'rawlist',
       name: 'css',
-      message: chalk.blue('\nCSS flavor: '),
+      message: chalk.blue('CSS flavor: '),
       choices: ['SCSS', 'Tailwind', 'BootstrapCDN'],
       filter(val) {
         return val.toLowerCase();
@@ -57,7 +57,7 @@ inquirer
     {
       type: 'rawlist',
       name: 'js',
-      message: chalk.yellow('\nWhat about Scripting: '),
+      message: chalk.yellow('What Scripting: '),
       choices: ['Javascript', 'TypeScript'],
       filter(val) {
         return val.toLowerCase();
@@ -207,6 +207,27 @@ inquirer
         
       }
 
+      //
+      //  --- TypeScript ---
+      //
+
+      /*
+      if (answers.js === 'typescript') {
+
+        try {
+
+          // FOLDER: src/scripts/ts
+
+          if (!fs.existsSync(folderName+'/src/scripts/ts', { recursive: true })) {
+            fs.mkdirSync(folderName+'/src/scripts/ts', { recursive: true });
+          }
+        } catch (err) {
+          console.error(err);
+        }
+        
+      }
+      */
+
 
       //
       // --- Static ---
@@ -215,7 +236,6 @@ inquirer
       try {
         if (!fs.existsSync(folderName+'/src/static', { recursive: true })) {
           fs.mkdirSync(folderName+'/src/static', { recursive: true });
-          console.log('/src/static created.')
         }
       } catch (err) {
         console.error(err);
@@ -231,7 +251,6 @@ inquirer
 
         if (!fs.existsSync(folderName+'/src/styles', { recursive: true })) {
           fs.mkdirSync(folderName+'/src/styles', { recursive: true });
- 
         }
       } catch (err) {
         console.error(err);
@@ -250,7 +269,6 @@ inquirer
 
           if (!fs.existsSync(folderName+'/src/styles/sass', { recursive: true })) {
             fs.mkdirSync(folderName+'/src/styles/sass', { recursive: true });
-
           }
         
           // FILE: src/styles/sass/sassy.scss
@@ -263,7 +281,6 @@ inquirer
           if (err) {
             console.error(err);
           }
-
         });
         
         } catch (err) {
@@ -285,20 +302,53 @@ inquirer
       // FILE: /src/layouts/base.njk
 
       let cssfile = '';
+      let jsfile = '';
+      let alpineStore = '';
+      let alpineCDN = '';
 
       if(answers.css === 'scss') {
-        cssfile = "<link rel=\"stylesheet\" src=\"css/sassy.css\" />";
+        cssfile = "<link rel=\"stylesheet\" src=\"css/main.css\" />";
       } else if (answers.css === 'bootstrapcdn') {
         cssfile = `
         <!-- Bootstrap CDN -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        <!-- Custom styles from src/styles/sass/sassy.scss -->
-        <link rel=\"stylesheet\" src=\"css/sassy.css\" />`
+        <!-- Custom styles from src/styles/sass/main.scss -->
+        <link rel=\"stylesheet\" src=\"css/main.css\" />`
       }  
       else if (answers.css === 'tailwind') {
         cssfile = "<link rel=\"stylesheet\" src=\"css/tw.css\" />"
       }  
+
+      if(answers.js === 'javascript') {
+        jsfile = "<script src=\"/script.js\"></script>";
+      } else {
+        jsfile = "<script src=\"/main.js\"></script>";
+      }
+
+      if(answers.js === 'typescript') {
+
+        alpineStore = `
+      <script>
+        // Alpine Data Store, included into all pages via <body x-data="data">
+        document.addEventListener('alpine:init', () => {
+          Alpine.data('data', ()  => ({
+
+              name: '${project}',
+
+              // Fired on x-init
+              async init() {
+
+              },
+                
+          })) // End Alpine.data('data')
+        }) // End EventListener
+      </script>`
+
+        alpineCDN = `
+        <!-- To use it as Module, Types need to be installed  -->
+        <script src="//unpkg.com/alpinejs" defer></script>`;
+      }
       
         const layoutBase = `
 <!DOCTYPE html>
@@ -312,12 +362,15 @@ inquirer
     <link rel="author" href="/humans.txt" />
     <link rel="icon" href="/favicon.ico" />
 
-<!-- Fonts for the Welcome page -->    
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Boogaloo&family=Caveat&display=swap" rel="stylesheet"> 
+    <!-- Fonts for the Welcome page -->    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Boogaloo&family=Caveat&display=swap" rel="stylesheet"> 
    
+    ${alpineCDN}
+       
     ${cssfile}
+
 
     {% block headStyles %}{% endblock %}
     {% block headScripts %}{% endblock %}
@@ -326,7 +379,9 @@ inquirer
 
         {% block main %}{% endblock %}
         
-    <script src="/script.js"></script>
+    ${jsfile}
+
+    ${alpineStore}
 
     </body>
 </html>`;
@@ -472,6 +527,140 @@ document.addEventListener('alpine:init', () => {
         });
 
         
+      }
+
+
+      //
+      // --- if TypeScript ---
+      //
+      
+      if (answers.js === 'typescript') {
+
+
+        // FILE: tsconfig.json
+
+        const tsconfigContent = `{
+          "compilerOptions": {
+            /* Visit https://aka.ms/tsconfig.json to read more about this file */
+        
+            /* Projects */
+            // "incremental": true,                              /* Enable incremental compilation */
+            // "composite": true,                                /* Enable constraints that allow a TypeScript project to be used with project references. */
+            // "tsBuildInfoFile": "./",                          /* Specify the folder for .tsbuildinfo incremental compilation files. */
+            // "disableSourceOfProjectReferenceRedirect": true,  /* Disable preferring source files instead of declaration files when referencing composite projects */
+            // "disableSolutionSearching": true,                 /* Opt a project out of multi-project reference checking when editing. */
+            // "disableReferencedProjectLoad": true,             /* Reduce the number of projects loaded automatically by TypeScript. */
+        
+            /* Language and Environment */
+            "target": "ES6",                                  /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations. */
+            // "lib": [],                                        /* Specify a set of bundled library declaration files that describe the target runtime environment. */
+            // "jsx": "preserve",                                /* Specify what JSX code is generated. */
+            // "experimentalDecorators": true,                   /* Enable experimental support for TC39 stage 2 draft decorators. */
+            // "emitDecoratorMetadata": true,                    /* Emit design-type metadata for decorated declarations in source files. */
+            // "jsxFactory": "",                                 /* Specify the JSX factory function used when targeting React JSX emit, e.g. 'React.createElement' or 'h' */
+            // "jsxFragmentFactory": "",                         /* Specify the JSX Fragment reference used for fragments when targeting React JSX emit e.g. 'React.Fragment' or 'Fragment'. */
+            // "jsxImportSource": "",                            /* Specify module specifier used to import the JSX factory functions when using \`jsx: react-jsx*\`.\` */
+            // "reactNamespace": "",                             /* Specify the object invoked for \`createElement\`. This only applies when targeting \`react\` JSX emit. */
+            // "noLib": true,                                    /* Disable including any library files, including the default lib.d.ts. */
+            // "useDefineForClassFields": true,                  /* Emit ECMAScript-standard-compliant class fields. */
+        
+            /* Modules */
+            "module": "commonjs",                                /* Specify what module code is generated. */
+            // "rootDir": "./",                                  /* Specify the root folder within your source files. */
+            // "moduleResolution": "node",                       /* Specify how TypeScript looks up a file from a given module specifier. */
+            // "baseUrl": "./",                                  /* Specify the base directory to resolve non-relative module names. */
+            // "paths": {},                                      /* Specify a set of entries that re-map imports to additional lookup locations. */
+            // "rootDirs": [],                                   /* Allow multiple folders to be treated as one when resolving modules. */
+            // "typeRoots": [],                                  /* Specify multiple folders that act like \`./node_modules/@types\`. */
+            // "types": [],                                      /* Specify type package names to be included without being referenced in a source file. */
+            // "allowUmdGlobalAccess": true,                     /* Allow accessing UMD globals from modules. */
+            // "resolveJsonModule": true,                        /* Enable importing .json files */
+            // "noResolve": true,                                /* Disallow \`import\`s, \`require\`s or \`<reference>\`s from expanding the number of files TypeScript should add to a project. */
+        
+            /* JavaScript Support */
+            // "allowJs": true,                                  /* Allow JavaScript files to be a part of your program. Use the \`checkJS\` option to get errors from these files. */
+            // "checkJs": true,                                  /* Enable error reporting in type-checked JavaScript files. */
+            // "maxNodeModuleJsDepth": 1,                        /* Specify the maximum folder depth used for checking JavaScript files from \`node_modules\`. Only applicable with \`allowJs\`. */
+        
+            /* Emit */
+            // "declaration": true,                              /* Generate .d.ts files from TypeScript and JavaScript files in your project. */
+            // "declarationMap": true,                           /* Create sourcemaps for d.ts files. */
+            // "emitDeclarationOnly": true,                      /* Only output d.ts files and not JavaScript files. */
+            // "sourceMap": true,                                /* Create source map files for emitted JavaScript files. */
+            // "outFile": "./",                                  /* Specify a file that bundles all outputs into one JavaScript file. If \`declaration\` is true, also designates a file that bundles all .d.ts output. */
+            "outDir": "./public",                               /* Specify an output folder for all emitted files. */
+            // "removeComments": true,                           /* Disable emitting comments. */
+            // "noEmit": true,                                   /* Disable emitting files from a compilation. */
+            // "importHelpers": true,                            /* Allow importing helper functions from tslib once per project, instead of including them per-file. */
+            // "importsNotUsedAsValues": "remove",               /* Specify emit/checking behavior for imports that are only used for types */
+            // "downlevelIteration": true,                       /* Emit more compliant, but verbose and less performant JavaScript for iteration. */
+            // "sourceRoot": "./src/scripts",                                 /* Specify the root path for debuggers to find the reference source code. */
+            // "mapRoot": "",                                    /* Specify the location where debugger should locate map files instead of generated locations. */
+            // "inlineSourceMap": true,                          /* Include sourcemap files inside the emitted JavaScript. */
+            // "inlineSources": true,                            /* Include source code in the sourcemaps inside the emitted JavaScript. */
+            // "emitBOM": true,                                  /* Emit a UTF-8 Byte Order Mark (BOM) in the beginning of output files. */
+            // "newLine": "crlf",                                /* Set the newline character for emitting files. */
+            // "stripInternal": true,                            /* Disable emitting declarations that have \`@internal\` in their JSDoc comments. */
+            // "noEmitHelpers": true,                            /* Disable generating custom helper functions like \`__extends\` in compiled output. */
+            // "noEmitOnError": true,                            /* Disable emitting files if any type checking errors are reported. */
+            // "preserveConstEnums": true,                       /* Disable erasing \`const enum\` declarations in generated code. */
+            // "declarationDir": "./",                           /* Specify the output directory for generated declaration files. */
+            // "preserveValueImports": true,                     /* Preserve unused imported values in the JavaScript output that would otherwise be removed. */
+        
+            /* Interop Constraints */
+            // "isolatedModules": true,                          /* Ensure that each file can be safely transpiled without relying on other imports. */
+            // "allowSyntheticDefaultImports": true,             /* Allow 'import x from y' when a module doesn't have a default export. */
+            "esModuleInterop": true,                             /* Emit additional JavaScript to ease support for importing CommonJS modules. This enables \`allowSyntheticDefaultImports\` for type compatibility. */
+            // "preserveSymlinks": true,                         /* Disable resolving symlinks to their realpath. This correlates to the same flag in node. */
+            "forceConsistentCasingInFileNames": true,            /* Ensure that casing is correct in imports. */
+        
+            /* Type Checking */
+            "strict": true,                                      /* Enable all strict type-checking options. */
+            // "noImplicitAny": true,                            /* Enable error reporting for expressions and declarations with an implied \`any\` type.. */
+            // "strictNullChecks": true,                         /* When type checking, take into account \`null\` and \`undefined\`. */
+            // "strictFunctionTypes": true,                      /* When assigning functions, check to ensure parameters and the return values are subtype-compatible. */
+            // "strictBindCallApply": true,                      /* Check that the arguments for \`bind\`, \`call\`, and \`apply\` methods match the original function. */
+            // "strictPropertyInitialization": true,             /* Check for class properties that are declared but not set in the constructor. */
+            // "noImplicitThis": true,                           /* Enable error reporting when \`this\` is given the type \`any\`. */
+            // "useUnknownInCatchVariables": true,               /* Type catch clause variables as 'unknown' instead of 'any'. */
+            // "alwaysStrict": true,                             /* Ensure 'use strict' is always emitted. */
+            // "noUnusedLocals": true,                           /* Enable error reporting when a local variables aren't read. */
+            // "noUnusedParameters": true,                       /* Raise an error when a function parameter isn't read */
+            // "exactOptionalPropertyTypes": true,               /* Interpret optional property types as written, rather than adding 'undefined'. */
+            // "noImplicitReturns": true,                        /* Enable error reporting for codepaths that do not explicitly return in a function. */
+            // "noFallthroughCasesInSwitch": true,               /* Enable error reporting for fallthrough cases in switch statements. */
+            // "noUncheckedIndexedAccess": true,                 /* Include 'undefined' in index signature results */
+            // "noImplicitOverride": true,                       /* Ensure overriding members in derived classes are marked with an override modifier. */
+            // "noPropertyAccessFromIndexSignature": true,       /* Enforces using indexed accessors for keys declared using an indexed type */
+            // "allowUnusedLabels": true,                        /* Disable error reporting for unused labels. */
+            // "allowUnreachableCode": true,                     /* Disable error reporting for unreachable code. */
+        
+            /* Completeness */
+            // "skipDefaultLibCheck": true,                      /* Skip type checking .d.ts files that are included with TypeScript. */
+            "skipLibCheck": true                                 /* Skip type checking all .d.ts files. */
+          }
+        }`;
+
+        fs.writeFile(folderName+'/tsconfig.json', tsconfigContent, err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+
+
+        // FILE: src/scripts/main.ts
+
+        const content = `//   Do the TS stuff here :)  
+        alert('Hello TypeScript!'); `;
+
+        fs.writeFile(folderName+'/src/scripts/main.ts', content, err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+
+
+     
       }
 
 
@@ -659,8 +848,8 @@ module.exports = {
       }
       if (answers.js === 'typescript') {
         tsScript = `
-        "w-ts": "npx tsc --src src/scripts/main.ts --outdir public/js/tscript.js --watch",
-        "b-ts": "tsc src/scripts/main.ts --outFile public/tscript.js",`
+        "w-ts": "npx tsc --watch",
+        "b-ts": "npx tsc && npx esbuild public/main.js --minify --outfile=public/main.min.js",`
       } else {
         tsScript = '';
       }
@@ -736,7 +925,7 @@ module.exports = {
 
         console.log('\n'+chalk.magenta('Ok '+ chalk.bold(answers.author) +', you Rock!'))
         console.log(chalk.bold.whiteBright('Go: ')+ 'cd '+project+' && npm i')
-        console.log(chalk.bold.whiteBright('then: ')+ 'npm run start')
+        console.log(chalk.bold.whiteBright('then: ')+ 'npm run dev')
 
         console.log('\n'+chalk.yellowBright.italic('Zou! Happy Coding!')+'\n')
 
